af9e143994abca8c1c251551f48c8c2b3a1f072f
Oleg Zybailov <oleg@Olegs-MacBook-Pro.local>
2026-04-25 22:39:50 +0200

    Updates

--- _next/static/6GgQuN0dieM0syGjGrnsq/_buildManifest.js
+++ _next/static/6GgQuN0dieM0syGjGrnsq/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/M360_C//_next/:path+",
-        "destination": "/M360_C/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
